f185a84819348d074eb6444766a0f2303f8e7e93
Jess <koda@horizons.gg>
2021-08-29 23:49:25 +1000

    Setup Page Start

--- install.bat
+++ install.bat
@@ -1,1 +1,3 @@
 npm i fs express discord.js node-fetch ejs
+node server.js install
+pause
--- server.js
+++ server.js
@@ -12,12 +12,30 @@ if (!fs.existsSync('local')) {
     //? Write Configs
     var toolboxConfig = {
         web: {
+            setup_complete: false,
             port: 8080
         }
     }
 
     fs.writeFileSync('./local/toolbox.json', JSON.stringify(toolboxConfig, null, '\t'))
+}
 
+//? Install Service
+if (process.argv[2] == 'install') {
+    exec('.\\util\\nssm.exe install "Sector Toolbox" .\\util\\start.bat', (err, stdout, stderr) => {
+        if (err) return console.error(err)
+    }).on('exit', code => {
+        console.log(`Service Successfully Installed | Code ${code}`)
+    })
+}
+
+//? Uninstall Service
+if (process.argv[2] == 'uninstall') {
+    exec('.\\util\\nssm.exe remove "Sector Toolbox"', (err, stdout, stderr) => {
+        if (err) return console.error(err)
+    }).on('exit', code => {
+        console.log(`Service Successfully Uninstalled | Code ${code}`)
+    })
 }
 
 
@@ -34,11 +52,17 @@ toolbox = require('./local/toolbox.json')
 //! Startup
 //!
 
-if (toolbox.service_enabled) {
-    //? Install Service
-    exec('.\\util\\nssm.exe install "Sector Toolbox" .\\util\\start.bat', (err, stdout, stderr) => {
-        if (err) return console.error(err)
-    }).on('exit', code => {
-        console.log('Process has exited with code ' + code)
-    })
-}
+//? Start Express
+const express = require('express')
+const app = express()
+const port = toolbox.web.port
+app.set('view engine', 'ejs')
+app.use(express.static('assets'))
+app.listen(port, () => {
+    if (!toolbox.web.setup_complete) exec('start http://localhost:' + port)
+})
+
+//? Load Homepage/Setup
+app.get('/', (req, res) => {
+    if (!toolbox.web.setup_complete) res.render('setup')
+})
