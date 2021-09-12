b56744f33cc6dc3e1a97c8e902dbe90bb682de3c
Jess <koda@horizons.gg>
2021-09-13 01:11:06 +1000

    Added RCON Password Generation

--- functions/controls.js
+++ functions/controls.js
@@ -5,7 +5,9 @@ const Discord = require('./discord')
 const { spawn } = require('child_process')
 
 const fs = require('fs')
-const Gamedig = require('gamedig');
+const Password = require('generate-password')
+const Gamedig = require('gamedig')
+const { isAnyArrayBuffer } = require('util/types')
 
 var Attached = {}
 
@@ -29,12 +31,19 @@ module.exports = {
             if (Tasklist.includes(`${id}.Server.exe`)) return
 
             Attached[id] = spawn(`${SystemConfig.system.directory}\\${id}\\${id}.Server.exe`, [], { detached: true }), Discord.Notification(`⏳ ${id} Starting...`, '#4273fc')
-            Attached[id].stdout.on('data', (data) => {
+            Attached[id].stdout.on('data', async (data) => {
                 data = data.toString()
                 console.log(data)
 
                 if (data.includes('Game ready')) {
                     Discord.Notification(`✅ ${id} is Ready to Join!`, '#33d438')
+                    if (config.rcon_password === 'sIHb6F4ew//D1OfQInQAzQ==') {
+                        var newPassword = Password.generate({ length: 24, numbers: true })
+                        await this.Rcon(id, 'system', [`!rcon setpassword ${newPassword}`, `!stop true 0`])
+                        config.rcon_password = newPassword
+                        await fs.promises.writeFile(`${SystemConfig.system.directory}\\${id}\\Torch.js\\config.json`, JSON.stringify(config, null, '\t')).catch(() => { console.log('Failed to update Password!'), this.Rcon(id, 'system', `!rcon setpassword password`) })
+                        Discord.Notification(`⚙️ RCON Password has been Generated for ${id}`, '#e06f28')
+                    }
                 }
 
                 if (data.includes('Server stopped.')) {
@@ -64,12 +73,12 @@ module.exports = {
         if (user !== 'system') config.online = false
         await fs.promises.writeFile(`${SystemConfig.system.directory}\\${id}\\Torch.js\\config.json`, JSON.stringify(config, null, '\t')).catch(() => { console.log('Could not write a Torch.js config file for this server.') })
 
-        if (!Attached[id] || force) return spawn(`powershell`, ['Stop-Process', `-Name ${id}.Server`]), console.log(`Killed ${id}`), Discord.Notification(`☠️ ${id}'s Process was Forcefully Killed`, '#400505')
+        if (!Attached[id] || force) return spawn(`powershell`, ['Stop-Process', `-Name ${id}.Server`]), console.log(`Killed ${id}`), Discord.Notification(`☠️ ${id}'s Process has been Forcefully Killed`, '#d43333')
         Gamedig.query({ type: 'spaceengineers', host: '127.0.0.1', port: config.port })
             .then(async (data) => {
-                this.Rcon('Sol', user, '!stop true 0'), console.log(`Stop Command issued to ${id}`), Discord.Notification(`⏳ Stop Request Issued to ${id}`, '#e06f28')
+                this.Rcon(id, user, ['!stop true 0']), console.log(`Stop Command issued to ${id}`), Discord.Notification(`⏳ Stop Request Issued to ${id}`, '#e06f28')
             }).catch(() => {
-                spawn(`powershell`, ['Stop-Process', `-Name ${id}.Server`]), console.log(`Killed ${id}`), Discord.Notification(`☠️ ${id}'s Process was Forcefully Killed`, '#400505')
+                spawn(`powershell`, ['Stop-Process', `-Name ${id}.Server`]), console.log(`Killed ${id}`), Discord.Notification(`☠️ ${id}'s Process has been Forcefully Killed`, '#d43333')
             });
     },
 
@@ -100,16 +109,28 @@ module.exports = {
         })
     },
 
-    Rcon: async function (id, user, command) {
+    Rcon: async function (id, user, commands) {
         if (!Permissions.Check(user, 'server.rcon')) return false
+        var config = await fs.promises.readFile(`${SystemConfig.system.directory}\\${id}\\Torch.js\\config.json`).catch(() => { console.log('Could not find a Torch.js config file for this server.') })
+        if (!config) return false
+        config = JSON.parse(config.toString())
+
+        if (config.rcon_password === 'sIHb6F4ew//D1OfQInQAzQ==') config.rcon_password = 'password'
 
         try {
             var { Rcon } = require("rcon-client")
             const rcon = await Rcon.connect({
-                host: "127.0.0.1", port: 27018, password: 'torchJSisCool'
+                host: "127.0.0.1", port: config.rcon, password: config.rcon_password
             })
-            var response = await rcon.send(command)
+
+            var response = []
+            for (command of commands) {
+                response.push(await rcon.send(command))
+            }
+
             rcon.on('error', (err) => console.log(err))
+
+            console.log(response)
 
             return response, rcon.end()
         } catch (e) {
--- functions/essentials.js
+++ functions/essentials.js
@@ -15,11 +15,42 @@ module.exports = {
     },
 
     UpdateFiles: async function (id) {
-        var config = await fs.promises.readFile(`${SystemConfig.system.directory}\\${id}\\Torch.js\\config.json`).catch(() => { console.log('Could not find a Torch.js config file for this server.') })
+        var config = await fs.promises.readFile(`${SystemConfig.system.directory}\\${id}\\Torch.js\\config.json`).catch((err) => console.log(err))
         if (!config) return false
         config = JSON.parse(config)
 
+        //? Plugins
+        var mandatory_plugins = ['c1b62cf3-9566-4cb5-97cc-d8d2368bc0a6', 'cbfdd6ab-4cda-4544-a201-f73efa3d46c0']
+        for (plugin of mandatory_plugins) {
+            if (!config.plugins.includes(plugin)) config.plugins.push(plugin)
+        }
+        var torch_cfg = (await fs.promises.readFile(`${SystemConfig.system.directory}\\${id}\\Torch.cfg`).catch((err) => console.log(err))).toString()
 
+        var plugins = '<Plugins>\n'
+        for (plugin of config.plugins) {
+            plugins += `\t\t<guid>${plugin}</guid>\n`
+        }
+        plugins += '\t</Plugins>'
+
+        if (torch_cfg.includes('<Plugins />')) torch_cfg = torch_cfg.replace('<Plugins />', plugins)
+        else {
+            this.ReplaceXml(torch_cfg, 'Plugins', plugins)
+        }
+
+        await fs.promises.writeFile(`${SystemConfig.system.directory}\\${id}\\Torch.cfg`, torch_cfg).catch((err) => console.log(err))
+
+
+        //? Rcon
+        await fs.promises.copyFile(`.\\resources\\plugins\\asset\\RconPlugin.zip`, `${SystemConfig.system.directory}\\${id}\\Plugins\\RconPlugin.zip`).catch((err) => console.log(err))
+        var rcon_cfg = (await fs.promises.readFile(`.\\resources\\plugins\\config\\Rcon.cfg`).catch(() => { console.log('Failed to Read Rcon.cfg!') })).toString()
+        if (fs.existsSync(`${SystemConfig.system.directory}\\${id}\\Instance\\Rcon.cfg`)) rcon_cfg = (await fs.promises.readFile(`${SystemConfig.system.directory}\\${id}\\Instance\\Rcon.cfg`).catch((err) => console.log(err))).toString()
+        
+        if (config.rcon_password === 'sIHb6F4ew//D1OfQInQAzQ==') rcon_cfg = this.ReplaceXml(rcon_cfg, 'PassHash', 'sIHb6F4ew//D1OfQInQAzQ==')
+        rcon_cfg = this.ReplaceXml(rcon_cfg, 'Port', config.rcon)
+        await fs.promises.writeFile(`${SystemConfig.system.directory}\\${id}\\Instance\\Rcon.cfg`, rcon_cfg).catch((err) => console.log(err))
+
+
+        return true
     }
 
 }
