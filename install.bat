199edcde235a1fedbdf443caeaea174faef3723e
Jess <koda@horizons.gg>
2021-09-16 06:06:25 +1000

    Updater

--- install.bat
+++ install.bat
@@ -1,5 +1,4 @@
 @echo off
-powershell npm i fs express discord.js node-fetch ejs public-ip multer cookie-parser socket.io http ncp rcon-client gamedig generate-password
-node server.js install
+powershell npm i fs express discord.js node-fetch@2 ejs public-ip multer cookie-parser socket.io http ncp rcon-client gamedig generate-password extract-zip
 start start.bat
 exit
